fe21d60a8d422a4347a422ea48b86614d5a43ff5
coma409 <grandyearly@hotmail.com>
2023-05-26 18:16:24 +0800

    Update index.js

--- src/muya/lib/renderers/index.js
+++ src/muya/lib/renderers/index.js
@@ -6,7 +6,6 @@ const rendererCache = new Map()
 const loadRenderer = async (name) => {
   if (!rendererCache.has(name)) {
     let m
-    let mindmap
     switch (name) {
       case 'sequence':
         m = await import('../parser/render/sequence')
@@ -23,11 +22,6 @@ const loadRenderer = async (name) => {
       case 'mermaid':
         // m = await import('mermaid/dist/mermaid.core.js')
         m = await import('mermaid/dist/mermaid.core.mjs')
-
-        mindmap = await import('@mermaid-js/mermaid-mindmap/dist/mermaid-mindmap.core.mjs')
-        rendererCache.set(`${name}_mindmap`, mindmap.default)
-        await m.default.registerExternalDiagrams([mindmap.default], { lazyLoad: false })
-
         rendererCache.set(name, m.default)
         break
       case 'vega-lite':
